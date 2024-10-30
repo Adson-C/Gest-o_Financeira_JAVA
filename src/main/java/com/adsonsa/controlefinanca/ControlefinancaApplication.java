04775a9e35b3359712a1f1e7f7aafcf9fd2fef13
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-10-29 21:51:20 -0300

    Iniciando Projeto - Class Usuario

---
+++ src/main/java/com/adsonsa/controlefinanca/ControlefinancaApplication.java
@@ -0,0 +1,13 @@
+package com.adsonsa.controlefinanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlefinancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlefinancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adsonsa/controlefinanca/ControlefinancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsonsa.controlefinanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlefinancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
